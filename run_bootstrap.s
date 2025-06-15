d038836e73db1c49a75202cf46123451eec94749
hyungrok-do <hyungrok-do@github.com>
2025-06-15 00:09:23 -0400

    fix

--- run_bootstrap.s
+++ run_bootstrap.s
@@ -6,8 +6,8 @@
 #SBATCH --mem=32GB
 #SBATCH --partition=a100_short
 #SBATCH --gres=gpu:a100:1
-#SBATCH --job-name=PLSI
-#SBATCH --output=logs/simulation_%02a.log
+#SBATCH --job-name=PLSI_bootstrap
+#SBATCH --output=logs/bootstrap_%02a.log
 #SBATCH -a 0-17
 
 module load singularity/3.9.8
@@ -31,4 +31,4 @@ echo "Running simulation with n=$n, g_fn=$g_fn, outcome=$outcome"
 
 singularity exec --nv --bind $SCRATCH --overlay $SCRATCH/containers/domain-generalization.ext3:ro \
     $SCRATCH/containers/cuda11.8.86-cudnn8.7-devel-ubuntu22.04.2.sif \
-    /ext3/miniconda3/bin/python test.py --n $n --g_fn $g_fn --outcome $outcome
+    /ext3/miniconda3/bin/python bootstrap.py --n $n --g_fn $g_fn --outcome $outcome
